5f8cb6e53cd31f962fbc9ceb8cd307cc0ed102fd
Raphaël LEJOLIVET <raphael.lejolivet@ensiie.fr>
2014-11-22 12:14:38 +0100

    Completed v1 of BuildingTemplate.cs Changing .gitignore to not tack some Unity/VStudio files.

--- Assets/Scripts/GameLogic/Base Elements (XML Serializable)/BuildingTemplate.cs
+++ Assets/Scripts/GameLogic/Base Elements (XML Serializable)/BuildingTemplate.cs
@@ -1,43 +1,114 @@
 ﻿using System.Xml.Serialization;
+using System.Collections.Generic;
+
+
+/**
+ * Template used for designers to give us their building ideas easily
+ * Imported from BuildingFactory, used to instantiate the Building class
+ **/
 
 [XmlRoot("BuildingTemplate")]
 [XmlType("LevelDescription")]
 public class BuildingTemplate {
 
-    [XmlAttribute]
+    [XmlElement("Name")]
     public string name
     {
         get;
         set;
     }
 
-    [XmlAttribute]
-    public float Cost
+    [XmlElement("Cost")]
+    public List<KeyValuePair<string, float>> ProxyCost
     {
-        get;
-        set;
+        get
+        {
+            return new List<KeyValuePair<string, float>>(this.Cost);
+        }
+        set
+        {
+            this.Cost = new Dictionary<string, float>();
+            foreach (var pair in value)
+                this.Cost[pair.Key] = pair.Value;
+        }
     }
 
-    /**
-     * Il manque pour l'instant la List<resource, int>
-     **/
+    private Dictionary<string, float> _cost;
 
-    [XmlAttribute]
+    [XmlIgnore]
+    public Dictionary<string, float> Cost
+    {
+        get
+        {
+            if (_cost == null)
+            {
+                _cost = new Dictionary<string, float>();
+            }
+            return _cost;
+        }
+        set
+        {
+            _cost = value;
+        }
+    }
+
+    [XmlElement("Incomes")]
+    public List<KeyValuePair<string, float>> ProxyIncomes
+    {
+        get
+        {
+            return new List<KeyValuePair<string, float>>(this.Incomes);
+        }
+        set
+        {
+            this.Incomes = new Dictionary<string, float>();
+            foreach (var pair in value)
+                this.Incomes[pair.Key] = pair.Value;
+        }
+    }
+
+    private Dictionary<string, float> _incomes;
+
+    [XmlIgnore]
+    public Dictionary<string, float> Incomes
+    {
+        get
+        {
+            if (_incomes == null)
+            {
+                _incomes = new Dictionary<string, float>();
+            }
+            return _incomes;
+        }
+        set
+        {
+            _incomes = value;
+        }
+    }
+
+    [XmlElement("Trade")]
     public float Trade
     {
         get;
         set;
     }
 
-    [XmlAttribute]
-    public float Recycle
+    [XmlElement("BuildTime")]
+    public float BuildTime
     {
         get;
         set;
     }
 
-    [XmlAttribute]
-    public float BuildTime
+    [XmlElement("Storage")]
+    public float Storage
+    {
+        get;
+        set;
+    }
+
+    [XmlElement("Description")]
+    public string Description
     {
         get;
         set;
